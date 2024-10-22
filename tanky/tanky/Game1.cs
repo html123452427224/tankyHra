c13c65789766bf851e619a0a7bac4aa447e02ba2
Jan Fuka <115619114+html123452427224@users.noreply.github.com>
2024-10-22 21:51:53 +0200

    tank nemuze odjet z okna a muze strilet

--- tanky/tanky/Game1.cs
+++ tanky/tanky/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace tanky
 {
@@ -12,6 +13,12 @@ namespace tanky
         private Vector2 Position;
         private Rectangle Rectangle;
         private Color Color;
+        private Rectangle projectile;
+        private Color projectileColor;
+        private Vector2 projectilePosition;
+        private Texture2D projectileTexture;
+        private Boolean shootProjectile;
+        private Vector2 projectileDireciton;
 
         public Game1()
         {
@@ -36,8 +43,13 @@ namespace tanky
             Rectangle = new Rectangle(50, 50, 50, 50);
             Position = new Vector2(10, 10);
             Texture = new Texture2D(GraphicsDevice, 1, 1);
-            Texture.SetData<Color>(new Color[] { Color});
-           
+            Texture.SetData<Color>(new Color[] { Color });
+            projectile = new Rectangle(0,0,10,10);
+            projectileColor = Color.Brown;
+            projectileTexture = new Texture2D(GraphicsDevice,1,1);
+            projectileTexture.SetData<Color>(new Color[] { projectileColor });
+
+
             // TODO: use this.Content to load your game content here
         }
 
@@ -46,26 +58,54 @@ namespace tanky
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
             var keyboardState = Keyboard.GetState();
+            
 
-            if (keyboardState.IsKeyDown(Keys.W))
+            if (keyboardState.IsKeyDown(Keys.W) && Position.Y > 0)
             {
                 Position.Y = Position.Y - 1;
             }
-            if (keyboardState.IsKeyDown(Keys.A))
+            if (keyboardState.IsKeyDown(Keys.A) && Position.X > 0)
             {
                 Position.X = Position.X - 1;
             }
 
-            if (keyboardState.IsKeyDown(Keys.S))
+            if (keyboardState.IsKeyDown(Keys.S) && Position.Y < _graphics.PreferredBackBufferHeight-Rectangle.Height)
             {
                 Position.Y = Position.Y + 1;
             }
 
-            if (keyboardState.IsKeyDown(Keys.D))
+            if (keyboardState.IsKeyDown(Keys.D) && Position.X < _graphics.PreferredBackBufferWidth - Rectangle.Width)
             {
                 Position.X = Position.X + 1;
             }
             // TODO: Add your update logic here
+         
+            var mouseState = Mouse.GetState();
+            Vector2 mousePosition = new Vector2(mouseState.X, mouseState.Y);
+            var direction = mousePosition - Position;
+            direction.Normalize();
+            if (keyboardState.IsKeyDown(Keys.Q) && !shootProjectile )
+            {
+                
+                shootProjectile = true;
+               
+                projectilePosition = Position;
+                projectileDireciton = direction;
+               
+
+
+
+            }
+           
+
+            if (shootProjectile)
+            {
+                projectilePosition += projectileDireciton * 5f;
+                if (projectilePosition.X < 0 || projectilePosition.X > _graphics.PreferredBackBufferWidth || projectilePosition.Y < 0 || projectilePosition.Y > _graphics.PreferredBackBufferHeight)
+                {
+                    shootProjectile = false;
+                }
+            }
 
             base.Update(gameTime);
         }
@@ -74,8 +114,15 @@ namespace tanky
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
-            _spriteBatch.Draw(Texture,Position,Rectangle,Color);
+            _spriteBatch.Draw(Texture, Position, Rectangle, Color);
             _spriteBatch.End();
+
+            if (shootProjectile)
+            {
+                _spriteBatch.Begin();
+                _spriteBatch.Draw(projectileTexture, projectilePosition, projectile, projectileColor);
+                _spriteBatch.End();
+            }
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
